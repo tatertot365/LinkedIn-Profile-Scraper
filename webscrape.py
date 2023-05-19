4750dab7df59d8d64cf3f89235ab5dfe18af3cc5
tatertot365 <tate.gillespie@gmail.com>
2023-05-18 18:56:04 -0600

    new and improved

--- webscrape.py
+++ webscrape.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from linkedin_scraper import Person, actions
 from creds import linkedin_username, linkedin_password, imported_profile_list
 
 # this logs into linkedin
@@ -13,7 +14,8 @@ def login():
     # This sets up the driver and opens the browser
     driver = webdriver.Chrome(options=opts, executable_path= "chromedriver")
 
-    driver.get("https://www.linkedin.com")
+    driver.get("https://www.linkedin.com/")
+    sleep(5)
 
     # username is submitedd
     username = driver.find_element(By.ID, 'session_key')
@@ -55,8 +57,12 @@ def get_soup(driver):
 
 # get name
 def get_name(soup):
+
     try:
         name = soup.find('h1', {'class': 'text-heading-xlarge'}).text
+
+        if name[-3:] in ['CFA', 'MBA']:
+            name = name[:-3]
     except:
         name = None
 
@@ -69,32 +75,38 @@ def get_name(soup):
     return name
 
 # get current position
-def get_current_position(soup, name):
-    try:
-        # navigate to the experience section
-        experience_section = soup.find('div', {'id': 'experience'})
-        jobs_div = experience_section.find_next('div', {'class': 'pvs-list__outer-container'}).findChildren()[0]
-        jobs_list = jobs_div.findChildren()
+def get_current_position(person, soup):
 
-        # loop through each list item and get the current position
-        jobs_array = []
-        for job in jobs_list:
-            potential = job.find_next('span', {'class': 'visually-hidden'}).text
+    if person is None:
+        try:
+            experience_section = soup.find('div', {'id': 'experience'})
+            jobs_div = experience_section.find_next('div', {'class': 'pvs-list__outer-container'}).findChildren()[0]
+            jobs_list = jobs_div.findChildren()
 
-            if potential not in jobs_array:
-                jobs_array.append(potential)
+            # loop through each list item and get the current position
+            jobs_array = []
+            for job in jobs_list:
+                potential = job.find_next('span', {'class': 'visually-hidden'}).text
 
-            if 'Present' in potential:
+                if potential not in jobs_array:
+                    jobs_array.append(potential)
 
-                ############################# This is where you need to add a name if it is not getting the position correctly #############################
-                if name in ["Corrine Richter", "Juan Jorge Poémape"]:
-                    current_position = jobs_array[-2]
-                else:
-                    current_position = jobs_array[-3]
-                break
+                if 'Present' in potential:
 
-    except:
-        current_position = None
+                    ############################# This is where you need to add a name if it is not getting the position correctly #############################
+                    if name in ["Corrine Richter", "Juan Jorge Poémape"]:
+                        current_position = jobs_array[-1]
+                    else:
+                        current_position = jobs_array[-3]
+                    break
+        except:
+            current_position = None
+
+    else:
+        try:
+            current_position = person.experiences[0].position_title
+        except:
+            current_position = None
 
     # clean up current position
     current_position = current_position.replace('\n', '')
@@ -105,17 +117,34 @@ def get_current_position(soup, name):
     return current_position
 
 # get current company
-def get_current_company(soup):
-    try:
-        current_company = soup.find('div', {'class': 'inline-show-more-text inline-show-more-text--is-collapsed inline-show-more-text--is-collapsed-with-line-clamp inline'}).text
-    except:
-        current_company = None
+def get_current_company(person, soup):
 
-    # clean up current company
-    current_company = current_company.replace('\n', '')
-    current_company = current_company.replace('"', '')
-    current_company = current_company.replace(',', '')
-    current_company = current_company.strip()
+    if person is None:
+        try:
+            current_company = soup.find('div', {'class': 'inline-show-more-text inline-show-more-text--is-collapsed inline-show-more-text--is-collapsed-with-line-clamp inline'}).text
+            current_company = current_company.replace('\n', '')
+            current_company = current_company.replace('"', '')
+            current_company = current_company.replace(',', '')
+            current_company = current_company.strip()
+        except:
+            current_company = None
+    else:
+        try:
+            current_company = person.experiences[0].institution_name
+
+            if current_company[-9:] in ["Full-time", "Part-time", "Freelance"]:
+                current_company = current_company[:-12]
+            elif current_company[-13:] == "Self-employed":
+                current_company = current_company[:-16]
+            elif current_company[-8:] in ["Contract", "Seasonal"]:
+                current_company = current_company[:-11]
+            elif current_company[-10:] == "Internship":
+                current_company = current_company[:-13]
+            elif current_company[-14:] == "Apprenticeship":
+                current_company = current_company[:-17]
+
+        except:
+            current_company = None
 
     return current_company
 
@@ -146,13 +175,13 @@ def get_graduation_year(soup):
     return graduation_year
 
 # this gets profile information from the linkedin profile page using beautiful soup
-def get_profile_data(soup):
+def get_profile_data(soup, person):
 
     # get the profile data calling the functions above
     # current_picture = get_current_picture(soup)
     name = get_name(soup)
-    current_company = get_current_company(soup)
-    current_position = get_current_position(soup, name)
+    current_position = get_current_position(person, soup)
+    current_company = get_current_company(person, soup)
     graduation_year = get_graduation_year(soup)
 
     # return [current_picture, name, current_position, current_company, graduation_year]
@@ -168,14 +197,21 @@ profile_data_df = pd.DataFrame(columns=['name', 'current_position', 'current_com
 # loop through each profile in the list
 for name in profile_list:
     driver.get("https://www.linkedin.com/in/" + name)
+    sleep(5)
+
     profile_url = "https://www.linkedin.com/in/" + name
-    sleep(8)
 
     # get the page source and parse it with beautiful soup
     soup = get_soup(driver)
+    sleep(5)
+
+    try:
+        person = Person(profile_url, driver=driver, close_on_complete=False)
+    except:
+        person = None
 
     # get the profile data and add it to the dataframe
-    profile_data = get_profile_data(soup)
+    profile_data = get_profile_data(soup, person)
     profile_data.append(profile_url)
     profile_data_df.loc[name] = profile_data
 
